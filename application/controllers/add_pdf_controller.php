b235060b7a203b461ccee93e392cc86cf8b66fe2
Martin Tapia <martindex@gmail.com>
2014-04-25 04:32:05 -0300

    + features Add PDF: se agrega campos en el formulario

--- application/controllers/add_pdf_controller.php
+++ application/controllers/add_pdf_controller.php
@@ -13,10 +13,22 @@ class Add_pdf_controller extends CI_Controller {
 		validateSession();
 		log_class_method(LEVEL_DEBUG, $this->className , 'index.....Inicio');		
 		$this->goAddForm();
+		log_class_method(LEVEL_DEBUG, $this->className , 'index.....Fin');
+	}
+	
+	public function add(){
+		log_class_method(LEVEL_DEBUG, $this->className, 'add.....Inicio');
+		$this->goSuccess();
+		log_class_method(LEVEL_DEBUG, $this->className, 'add.....Fin');
 	}
 	
 	private function goAddForm(){
 		log_class_method(LEVEL_DEBUG, $this->className, 'goAddForm');
-		$this->load->view('add_pdf_view');
+		$this->load->view('add_pdf_view', array('errorMessage'=>''));
+	}
+	
+	private function goSuccess(){
+		log_class_method(LEVEL_DEBUG, $this->className, 'goSuccess');
+		$this->load->view('success_view');		
 	}
 }
--- application/views/add_pdf_view.php
+++ application/views/add_pdf_view.php
@@ -4,10 +4,48 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>ConsultApp</title>
 </head>
-
+<?php
+//valores por defecto
+$attr_form = array('name' => 'add-pdf-form', 'id' => 'add-pdf-form', 'method'=>'POST');
+$attr_code = array('id' => 'code-field','name' => 'code-field','placeholder' => 'Codigo');
+$attr_name = array('id' => 'name-field','name' => 'name-field','placeholder' => 'Nombre');
+$attr_day = array('id' => 'date-field','name' => 'date-field','placeholder' => 'Dia');
+$attr_month = array('id' => 'date-field','name' => 'date-field','placeholder' => 'Mes');
+$attr_year = array('id' => 'date-field','name' => 'date-field','placeholder' => 'A&#241;o');
+$attr_select_file = array('id' => 'select-button','name' => 'select-button','value' => 'Seleccionar Archivo...');
+$attr_submit = array('id' => 'submit-button','name' => 'submit-button','value' => 'Guardar');
+?>
 <body>
 	<?php include 'menu.php';?>
-	
-	Add pdf view
+		
+	<div id="error-container">
+		<?=validation_errors();?>
+		<p><?=$errorMessage;?></p>
+	</div>
+	<div id="form-container">
+	<?=form_open(site_url().'add_pdf_controller/add',$attr_form);?>
+		<div>
+		<?=form_input($attr_code);?>
+		</div>
+		<div>
+		<?=form_input($attr_name);?>
+		</div>
+		<div>
+		<?=form_input($attr_day);?>
+		</div>
+		<div>
+		<?=form_input($attr_month);?>
+		</div>
+		<div>
+		<?=form_input($attr_year);?>
+		</div>
+		<div>
+		<?=form_submit($attr_select_file);?>
+		</div>
+		<div>
+		<?=form_submit($attr_submit);?>
+		</div>
+	<?=form_close();?>
+	</div>
 </body>
 </html> 
